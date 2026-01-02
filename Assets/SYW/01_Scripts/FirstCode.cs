e1b28e71020569be089d26215ba29c280b8e0de4
yeonwoo0981 <0981syw@naver.com>
2026-01-03 00:43:01 +0900

    First trash throw

---
+++ Assets/SYW/01_Scripts/FirstCode.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using DG.Tweening;
+
+namespace SYW._01_Scripts
+{
+    public class FirstCode : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.transform.DOScale(new Vector3(2.5f, 2.5f, 2.5f), 1.2f)
+                .SetLoops(10, LoopType.Yoyo);
+        }
+    }
+}
